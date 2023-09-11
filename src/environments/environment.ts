52630a4bdfcdf4dfbf0795096e1b1add45d1494d
Frederik Byl <frederik.byl@vito.be>
2023-09-11 10:05:20 +0200

    pointing to we are endpoint for authentication

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,18 +9,9 @@ export const environment = {
   backend_login: "/login",
   backend_read: "/read",
   backend_write: "/write",
-  weare_backend_base: "http://localhost:3003",
+  weare_backend_base: "https://app-api.we-are-acc.vito.be",
+  //weare_backend_base: "http://localhost:3003",
   backend_get_pods: "/pods",
-  //weare_backend_base: "https://app-api.we-are-acc.vito.be",
   weare_backend_login: "/login",
   demo_utils_hostname: "http://localhost:8080",
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
